4958c3a60fe05e694897c19bb3524dcfe3359e57
Prasenjit Purohit <github@prasenjit.net>
2020-03-08 00:40:00 +0530

    Initial Commit

---
+++ src/controllers/ping.controller.ts
@@ -0,0 +1,56 @@
+import {Request, RestBindings, get, ResponseObject} from '@loopback/rest';
+import {inject} from '@loopback/context';
+import {authenticate} from "@loopback/authentication";
+import { SecurityBindings, UserProfile } from '@loopback/security';
+
+/**
+ * OpenAPI response for ping()
+ */
+const PING_RESPONSE: ResponseObject = {
+  description: 'Ping Response',
+  content: {
+    'application/json': {
+      schema: {
+        type: 'object',
+        title: 'PingResponse',
+        properties: {
+          greeting: {type: 'string'},
+          date: {type: 'string'},
+          url: {type: 'string'},
+          headers: {
+            type: 'object',
+            properties: {
+              'Content-Type': {type: 'string'},
+            },
+            additionalProperties: true,
+          },
+        },
+      },
+    },
+  },
+};
+
+/**
+ * A simple controller to bounce back http requests
+ */
+export class PingController {
+  constructor(@inject(RestBindings.Http.REQUEST) private req: Request,@inject(SecurityBindings.USER)
+  private userProfile: UserProfile,) {}
+
+  // Map to `GET /ping`
+  @get('/ping', {
+    responses: {
+      '200': PING_RESPONSE,
+    },
+  })
+  @authenticate('basic')
+  ping(): object {
+    // Reply with a greeting, the current time, the url, and request headers
+    return {
+      greeting: 'Hello from LoopBack',
+      date: new Date(),
+      url: this.req.url,
+      headers: Object.assign({}, this.req.headers),
+    };
+  }
+}
